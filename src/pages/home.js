e2fe50e42d9c9204ec62d883d8b9a5dc1c7815cf
Kirill <mustang.horse34@gmail.com>
2023-04-06 19:02:30 +0700

    Added pagination

--- src/pages/home.js
+++ src/pages/home.js
@@ -32,6 +32,36 @@ const Home = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error!</p>;
 
-  return <NoteFeed notes={data.noteFeed.notes} />;
+  return (
+    <>
+      <NoteFeed notes={data.noteFeed.notes} />
+      {data.noteFeed.hasNextPage && (
+        <Button
+          onClick={() =>
+            fetchMore({
+              variables: {
+                cursor: data.noteFeed.cursor,
+              },
+              updateQuery: (previousResult, { fetchMoreResult }) => {
+                return {
+                  noteFeed: {
+                    cursor: fetchMoreResult.noteFeed.cursor,
+                    hasNextPage: fetchMoreResult.noteFeed.hasNextPage,
+                    notes: [
+                      ...previousResult.noteFeed.notes,
+                      ...fetchMoreResult.noteFeed.notes,
+                    ],
+                    __typename: 'noteFeed',
+                  },
+                };
+              },
+            })
+          }
+        >
+          Load more
+        </Button>
+      )}
+    </>
+  );
 };
 export default Home;
